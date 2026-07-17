0d2c3dd70a3dc36e16a73f2836b1c85570b69a4c
Wil Zhou <smithy@pr1ncess.net>
2026-07-17 03:12:05 -0400

    Set up vimrc and colorscheme via home manager, hyprland config file

--- users/will/home.nix
+++ users/will/home.nix
@@ -3,6 +3,13 @@
     ../../home/core.nix
   ];
 
+  home.file.".vim/colors/badwolf.vim".source = ./config/vim/colors/badwolf.vim;
+  home.file.".vimrc".source = ./config/vim/vimrc;
+
+  home.packages = with pkgs; [
+    vimPlugins.vim-pathogen
+  ];
+
   home.username = "will";
   home.homeDirectory = "/home/will";
 }
