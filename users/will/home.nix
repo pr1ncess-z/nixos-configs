2fb42ac53664bfa5b5f1e90e0b17da4cfa6ba9fa
Wil Zhou <smithy@pr1ncess.net>
2026-07-17 03:14:21 -0400

    Add hyprland.lua filepath to home manager

--- users/will/home.nix
+++ users/will/home.nix
@@ -5,6 +5,7 @@
 
   home.file.".vim/colors/badwolf.vim".source = ./config/vim/colors/badwolf.vim;
   home.file.".vimrc".source = ./config/vim/vimrc;
+  home.file.".config/hypr/hyprland.lua".source = ../../hosts/durian/config/hypr/hyprland.lua;
 
   home.packages = with pkgs; [
     vimPlugins.vim-pathogen
